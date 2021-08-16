ed75be834b494afc43a7d1aa897e954ce6aca5e4
Joshua inyang <joshuainyang255@gmail.com>
2021-08-16 23:40:30 +0100

    first commit

---
+++ bot.py
@@ -0,0 +1,37 @@
+from config import *
+
+@bot.message_handler(commands=['start', 'help'])
+def greet(msg):
+    bot.reply_to(msg, "hello " + str(msg.from_user.username))
+
+@bot.message_handler(commands=['contentmessage'])
+def start(msg):
+    if msg.from_user.id != ADMIN:
+
+        bot.reply_to(msg, "Not uncle B!!!!")
+
+
+    else:
+
+        bot.reply_to(msg, "Yes Boss!")
+
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Your bot application is still active!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='/' + TOKEN)
+    return "Application running!", 200
+
+
+if __name__ == "__main__":
+    
+    server.run(host="0.0.0.0", threaded=True, port=int(os.environ.get('PORT', 5000)))
+    
+        
---
+++ config.py
@@ -0,0 +1,20 @@
+import telebot
+from flask import Flask, request
+import os 
+
+
+
+# Logging Setup
+import logging
+logging.basicConfig(
+    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+    level=logging.WARNING
+    )
+
+
+TOKEN = "1920624938:AAEAsqDHsKqz0-QyIsb0qu5une0mvLVCLQw"
+ADMIN =  1190069449 
+
+
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
